d95bc0d93055162f819360a2b66cf86d3ace3659
Benjamin De la Cruz MArtinez <chomi.numb.94@gmail.com>
2019-02-26 22:23:23 -0600

    :sparkles: Add routes of error customs server

--- src/api/routes.cr
+++ src/api/routes.cr
@@ -1,10 +1,24 @@
 require "./users.cr"
+require "../helpers/routes-exclude"
 
 module ApiRoutes
   class Router
     def initialize(@route : String)
       static_headers do |response, filepath, filestat|
         response.headers.add("Access-Control-Allow-Origin", "*")
+      end
+
+      error 401 do
+        {message: "Not authorization", status: 401}.to_json
+      end
+
+      error 400 do
+        {message: "Error on params", status: 400}.to_json
+      end
+
+      before_all do |env|
+        ExcludeRoutes.validateSession(env)
+        # ExcludeRoutes.setAuth(env)
       end
 
       Users.setPathApi(@route)
