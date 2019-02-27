931dac1e0db3436ce11b83332991a9b028e10002
Benjamin De la Cruz MArtinez <chomi.numb.94@gmail.com>
2019-02-26 22:22:53 -0600

    :zap: :sparkles: Add class's with service of project

--- src/serviciosK.cr
+++ src/serviciosK.cr
@@ -1,6 +1,18 @@
 require "kemal"
+require "redis"
+
+require "./helpers/constant.cr"
+require "./helpers/generateToken.cr"
 require "./server.cr"
+require "./database/mysql.cr"
+require "./database/redis.cr"
+require "./helpers/authentication.cr"
+require "./helpers/validations.cr"
+
+Database.connect
+require "./database/models/users.cr"
+
+REDIS = RedisDatabase.connect
 
 ServerSK = Server::KemalServer.new
-
 ServerSK.run
