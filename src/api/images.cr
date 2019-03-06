9cc3a3c34267173cd2d07f8a58dc419d051e4da8
Benjamin De la Cruz MArtinez <chomi.numb.94@gmail.com>
2019-03-05 22:07:29 -0600

    :sparkles: :fire: Agregado cambio en endpoint para imagenes de perfil, se agrego validacion.

--- src/api/images.cr
+++ src/api/images.cr
@@ -19,7 +19,7 @@ class Images
       {result: response_result["url"]}.to_json
     end
 
-    post "#{url}/images/shop/avatar" do |env|
+    post "#{url}/images/avatar" do |env|
       response_result = ""
       HTTP::FormData.parse(env.request) do |upload|
         result_cover = CLOUDINARY.upload(upload, "avatar")
--- src/helpers/constant.cr
+++ src/helpers/constant.cr
@@ -6,4 +6,7 @@ ROUTES_EXCLUDE = [
 ONLY_ROUTES = [
   "/api/users/profile",
   "/api/shop",
+  "/api/images/avatar",
+  "/api/images/shop/logo",
+  "/api/images/shop/cover",
 ]
