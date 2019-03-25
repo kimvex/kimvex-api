4f776ce9c9f20635f3ed71d03b4717930ac4c9ef
Benjamin De la Cruz MArtinez <chomi.numb.94@gmail.com>
2019-03-24 21:53:20 -0600

    :recycle: Moviendo de lugar la incializacion de mongodb

--- src/init.cr
+++ src/init.cr
@@ -1,6 +1,3 @@
-require "./database/mongodb.cr"
-MONGO = MongoDB.new("", "kmv")
-
 require "kemal"
 require "redis"
 require "http"
@@ -10,6 +7,7 @@ require "./helpers/generateToken.cr"
 require "./server.cr"
 require "./database/mysql.cr"
 require "./database/redis.cr"
+require "./database/mongodb.cr"
 require "./helpers/authentication.cr"
 require "./helpers/validations.cr"
 require "./helpers/cloudinary.cr"
@@ -17,6 +15,7 @@ require "./helpers/cloudinary.cr"
 DB_K       = Database.new
 REDIS      = RedisDatabase.connect
 CLOUDINARY = Cloudinary.new("766496458317643", "h27hacklab")
+MONGO      = MongoDB.new("", "kmv")
 
 ServerSK = Server::KemalServer.new
 ServerSK.run
